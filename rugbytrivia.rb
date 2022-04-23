6159f8f38aa617182a588a61f9d0d2c447a70b5b
nakalevu <88434574+nakalevu@users.noreply.github.com>
2022-04-23 13:39:23 +1000

    Class for trivia app added

---
+++ rugbytrivia.rb
@@ -0,0 +1,10 @@
+# Class for the rugby trivia app
+class Trivia
+
+    def initialize
+# Letters for the user to select will be stored to an array
+        letters = ("a".."z").to_a
+        question = questions.sample
+    end
+
+end
